675b921f117e89712093f7fff01bcf73b563b776
Andreas Neubauer <Andreas4231@gmx.at>
2023-03-23 09:26:54 +0100

    Added readme and coker-compose

--- backend/launcher.py
+++ backend/launcher.py
@@ -3,6 +3,8 @@
 
     Launcher of the flask app!
 '''
+# Imports from default libraries
+import os
 
 # Imports from the project
 from app import create_app
@@ -10,4 +12,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port = os.environ.get("FLASK_SERVER_PORT", 8000), debug=True)
